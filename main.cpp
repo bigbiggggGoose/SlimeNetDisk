dcae7928920ca9476d4bfc007f6fa4a603bcbd95
bigbiggggGoose <114298933+bigbiggggGoose@users.noreply.github.com>
2025-07-30 17:38:46 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+ #include "mainwindow.h"
+ #include "logindialog.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //MainWindow w;
+    //w.show();
+    LoginDialog l;
+    l.show();
+    return a.exec();
+}
