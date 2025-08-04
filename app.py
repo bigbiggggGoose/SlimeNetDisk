c0696b4b2cf8f8a4ccca2fe6f95d696a404cfa96
bigbiggggGoose <114298933+bigbiggggGoose@users.noreply.github.com>
2025-08-04 20:03:28 +0800

    Add files via upload
    补上了update相关的东西

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ api.add_resource(file.FileDelete, '/dealfile/del')
 api.add_resource(ShareList, '/sharelist')
 api.add_resource(file.FileToggleShare, '/dealfile/toggle_share')
 
+@app.route('/uploads/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_DIR'], filename, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
